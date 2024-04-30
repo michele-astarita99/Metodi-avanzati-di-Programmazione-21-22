784ab7418b8baa1e45bb1a6e5b5999cbfb313cc8
michele-astarita99 <101122965+michele-astarita99@users.noreply.github.com>
2024-04-30 16:56:16 +0200

    Add Jar e Bat/client

---
+++ Jar e Bat/client/client.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "C:\Users\dario\Desktop\Progetto\Progetto knn con estensione\Jar e Bat\client"
+java --module-path "C:\Users\dario\Desktop\javafx-sdk-19\lib" --add-modules javafx.controls,javafx.fxml -jar client.jar
+pause
